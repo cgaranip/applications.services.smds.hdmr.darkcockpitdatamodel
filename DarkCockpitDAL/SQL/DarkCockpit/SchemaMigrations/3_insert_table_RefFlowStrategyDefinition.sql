59add175ce5968c10c7f3a301bfb81d38b890114
Swapnil Agrawal <swapnil.agrawal@intel.com>
2020-06-04 16:09:28 +0530

    US680375: Add SDA version creation and model compute into Autonomous PRF process

--- DarkCockpitDAL/SQL/DarkCockpit/SchemaMigrations/3_insert_table_RefFlowStrategyDefinition.sql
+++ DarkCockpitDAL/SQL/DarkCockpit/SchemaMigrations/3_insert_table_RefFlowStrategyDefinition.sql
@@ -1,6 +1,14 @@
 INSERT INTO [DarkCockpit].[dbo].[RefFlowStrategyDefinition](WorkFlowId, SubscriptionTopic, PublishTopic, ActionURL, ActionURLArgsJSON, SendEmail)
-SELECT 8, 'RunHDMR', 'None', NULL, NULL, 0
-UNION ALL
+SELECT T.WorkFlowId, T.SubscriptionTopic, T.PublishTopic, T.ActionURL, T.ActionURLArgsJSON, T.SendEmail
+FROM
+(
+SELECT 8 AS WorkFlowId, 'RunHDMR' AS SubscriptionTopic, 'None' AS PublishTopic, NULL AS ActionURL, NULL AS ActionURLArgsJSON, 0 AS SendEmail
+UNION 
 SELECT 8, 'CompletedRunHDMR', 'None', NULL, NULL, 0
-UNION ALL
+UNION 
 SELECT 8, 'ExceptionRunHDMR', 'None', NULL, NULL, 0
+) T
+LEFT JOIN [DarkCockpit].[dbo].[RefFlowStrategyDefinition] R
+	ON R.WorkFlowId = T.WorkFlowId
+	AND R.SubscriptionTopic = T.SubscriptionTopic
+	WHERE R.SubscriptionTopic IS NULL
